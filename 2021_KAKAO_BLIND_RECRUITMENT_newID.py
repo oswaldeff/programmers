80d78feb627e5c167a0fb7d6a0f6ebc4d2035dfa
owaldeff <tialem94@gmail.com>
2021-05-07 23:52:01 +0900

    step 4 ...ing

--- 2021_KAKAO_BLIND_RECRUITMENT_newID.py
+++ 2021_KAKAO_BLIND_RECRUITMENT_newID.py
@@ -10,16 +10,24 @@ def solution(new_id):
     print("2: ", new_id)
     
     # 3
-    new_id = re.sub('[.+]', '.', new_id)
+    det = True
+    while det:
+        prev = new_id
+        new_id = new_id.replace('..', '.')
+        if prev == new_id:
+            det = False
     print("3: ", new_id)
+    
     # 4
-    #new_id = list(new_id)
-    #print(new_id[0])
-    #print(new_id[-1])
-    #if new_id[0] == '.':
-    #    del new_id[0]
-    #if new_id[-1] == '.':
-    #    del new_id[-1]
+    new_id = list(new_id)
+    print(new_id[0])
+    print(new_id[-1])
+    if new_id[0] == '.':
+        print('1Pass', new_id)
+        del new_id[0]
+    if new_id[-1] == '.':
+        print('2Pass', new_id)
+        #del new_id[-1]
     #new_id = new_id.join
     print("4: ", new_id)
     
