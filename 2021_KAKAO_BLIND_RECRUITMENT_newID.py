7359f3709f22c37246b55650770bdcf2803acb88
owaldeff <tialem94@gmail.com>
2021-05-08 01:48:25 +0900

    solved

--- 2021_KAKAO_BLIND_RECRUITMENT_newID.py
+++ 2021_KAKAO_BLIND_RECRUITMENT_newID.py
@@ -3,11 +3,9 @@ import re
 def solution(new_id):
     # 1
     new_id = new_id.lower()
-    print("1: ", new_id)
     
     # 2
     new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
-    print("2: ", new_id)
     
     # 3
     det = True
@@ -16,22 +14,36 @@ def solution(new_id):
         new_id = new_id.replace('..', '.')
         if prev == new_id:
             det = False
-    print("3: ", new_id)
     
     # 4
-    new_id = list(new_id)
-    print(new_id[0])
-    print(new_id[-1])
-    if new_id[0] == '.':
-        print('1Pass', new_id)
-        del new_id[0]
-    if new_id[-1] == '.':
-        print('2Pass', new_id)
-        #del new_id[-1]
-    #new_id = new_id.join
-    print("4: ", new_id)
+    det = True
+    while det:
+        front = new_id[:1]
+        if front == '.':
+            new_id = new_id[1:]
+        end = new_id[-1:]
+        if end == '.':
+            new_id = new_id[:-1]
+        if front != '.' and end != '.':
+            det = False
     
     # 5
+    if len(new_id) == 0:
+        new_id += 'a'
     
-    answer = ''
+    # 6
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+    if new_id[-1:] == '.':
+        new_id = new_id[:-1]
+    
+    # 7
+    det = True
+    while det:
+        if len(new_id) <= 2:
+            new_id += new_id[-1:]
+        if len(new_id) > 2:
+            det = False
+    
+    answer = new_id
     return answer
