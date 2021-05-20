30d01d3a69e14ca1b69b145c93f779070d69976d
owaldeff <tialem94@gmail.com>
2021-05-20 21:37:13 +0900

    solving 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL

--- 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
+++ 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
@@ -2,11 +2,19 @@ from collections import defaultdict
 from itertools import combinations
 
 def solution(orders, course):
-    comb = defaultdict(list)
+    comb_l = defaultdict(list)
     for idx_s, order in enumerate(orders):
         order_list = list(order)
         for idx_l in range(2, len(order_list)):
-            comb[idx_s].extend(list(combinations(order_list, idx_l)))
-    print(comb)
+            element = list(map(''.join, combinations(order_list, idx_l)))
+            comb_l[idx_l].extend(element)
+    print('comb_l: ', comb_l)
+    
+    
+    comb_c = defaultdict(list)
+    for c in course:
+        comb_c[c] = set(comb_l[c])
+    print('comb_c: ', comb_c)
+    
     answer = []
     return answer
