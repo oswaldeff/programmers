f4e722b8990858cf4cf154cdba07dcd72c3e44ff
owaldeff <tialem94@gmail.com>
2021-05-21 23:20:27 +0900

    solving... 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL :testcase2

--- 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
+++ 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
@@ -4,7 +4,7 @@ from itertools import combinations
 def solution(orders, course):
     comb_l = defaultdict(list)
     for idx_s, order in enumerate(orders):
-        order_list = list(order)
+        order_list = sorted(list(order))
         for idx_l in range(2, len(order_list)):
             element = list(map(''.join, combinations(order_list, idx_l)))
             comb_l[idx_l].extend(element)
@@ -16,5 +16,14 @@ def solution(orders, course):
         comb_c[c] = set(comb_l[c])
     print('comb_c: ', comb_c)
     
+    comb_cnt = defaultdict(int)
+    for c in course:
+        for comb in comb_c[c]:
+            if comb in comb_l[c]:
+                cnt = comb_l[c].count(comb)
+                comb_cnt[comb] = cnt
+    print('comb_cnt: ', comb_cnt)
+    #comb_cnt = sorted(comb_cnt, key=lambda x: dict[x])
+    print('comb_cnt.sort: ', comb_cnt)
     answer = []
     return answer
