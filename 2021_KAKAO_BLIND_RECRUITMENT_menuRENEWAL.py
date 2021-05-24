0fb5f448c2fe72bcb6faf2e709f9d08cac6f491d
owaldeff <tialem94@gmail.com>
2021-05-24 23:18:26 +0900

    solving... only remains answer.append targets

--- 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
+++ 2021_KAKAO_BLIND_RECRUITMENT_menuRENEWAL.py
@@ -1,9 +1,26 @@
 from collections import defaultdict
+from itertools import combinations
 
 def solution(orders, course):
-    orders = sorted(orders, key=len)
-    print(orders)
-    for order in orders:
-        pass
+    comb_list = []
+    for idx_s, order in enumerate(orders):
+        order_list = sorted(list(order))
+        for idx_l in range(2, len(order_list)+2):
+            element = list(map(''.join, combinations(order_list, idx_l)))
+            comb_list.extend(element)
+    comb_list = sorted(comb_list)
+    comb_list = sorted(comb_list, key=len)
+    print('comb_list: ', comb_list)
+    comb_set = sorted(list(set(comb_list)))
+    comb_set = sorted(comb_set, key=len)
+    print('comb_set: ', comb_set)
+    comb_dict = defaultdict(int)
+    cnt = 0
+    for comb1 in comb_set:
+        for comb2 in comb_list:
+            if comb1 == comb2:
+                comb_dict[comb1] += 1
+    print('comb_dict: ', comb_dict)
+    
     answer = []
     return answer
