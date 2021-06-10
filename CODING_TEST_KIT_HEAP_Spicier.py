71e8b8d93699de0550af87c975bfdce8816f1eb6
owaldeff <tialem94@gmail.com>
2021-06-10 23:59:16 +0900

    test case pass, but efficiency test unpass

--- CODING_TEST_KIT_HEAP_Spicier.py
+++ CODING_TEST_KIT_HEAP_Spicier.py
@@ -5,8 +5,8 @@ def mix_scoville(scoville, K):
             cnt = -1
             break
         scoville.sort()
-        min_degree = scoville[0]
-        second_degree = scoville[1]
+        min_degree = scoville.pop(0)
+        second_degree = scoville.pop(0)
         mix_degree = min_degree+(second_degree*2)
         scoville.append(mix_degree)
         cnt += 1
