b4033a279d6620f8616d6294f157794d04fdcd90
owaldeff <tialem94@gmail.com>
2021-06-13 21:17:51 +0900

    using heapq: heap structure

--- CODING_TEST_KIT_HEAP_Spicier.py
+++ CODING_TEST_KIT_HEAP_Spicier.py
@@ -1,19 +1,19 @@
+import heapq as hq
+
 def mix_scoville(scoville, K):
     cnt = 0
     while min(scoville) < K:
         if len(scoville) == 1:
             cnt = -1
             break
-        min_degree = min(scoville)
-        scoville.remove(min_degree)
-        second_degree = min(scoville)
-        scoville.remove(second_degree)
+        min_degree = hq.heappop(scoville)
+        second_degree = hq.heappop(scoville)
         mix_degree = min_degree+(second_degree*2)
-        scoville.append(mix_degree)
+        hq.heappush(scoville, mix_degree)
         cnt += 1
     return cnt
 
 def solution(scoville, K):
-    scoville.sort()
+    hq.heapify(scoville)
     answer = mix_scoville(scoville, K)
     return answer
