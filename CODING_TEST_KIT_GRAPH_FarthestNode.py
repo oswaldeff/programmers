123475cd1446c633353c98a2f526a6247a6d2cba
owaldeff <tialem94@gmail.com>
2021-06-20 21:22:28 +0900

    Using DFS

--- CODING_TEST_KIT_GRAPH_FarthestNode.py
+++ CODING_TEST_KIT_GRAPH_FarthestNode.py
@@ -1,9 +1,19 @@
 from collections import defaultdict
+from collections import deque as dq
 def solution(n, edge):
-    graph = defaultdict(list)
+    graph = defaultdict(dq)
     for nodes in edge:
         graph[nodes[0]].append(nodes[1])
         graph[nodes[1]].append(nodes[0])
     print(graph)
+    
+    nodes = [x for x in range(1, n+1)]
+    print(nodes)
+    
+    # for node in nodes:
+    #     visited = dq()
+    #     while visited != graph[node]:
+    #         start_node = graph[node].popleft()
+            
     answer = 0
     return answer
