5dfbec5900cf488beee507110ea08491d27bcae0
osawldeff <tialem94@gmail.com>
2021-12-14 22:53:44 +0900

    solving...

--- 2020_KAKAO_INTERNSHIP_PushKeypad.py
+++ 2020_KAKAO_INTERNSHIP_PushKeypad.py
@@ -1,24 +1,39 @@
-keypad = {
-        '*': [0, 7],
-        '#': [0, 9],
-        0: ['*', '#', 8],
-        1: [2, 4],
-        2: [1, 3, 5],
-        3: [2, 6],
-        4: [1, 5, 7],
-        5: [2, 4, 6, 8],
-        6: [3, 5, 9],
-        7: ['*', 4, 8],
-        8: [0, 5, 7, 9],
-        9: ['#', 6, 8]
+def solution(numbers, hand):
+    keypad = {
+        '*': (0, 7),
+        '#': (0, 9),
+        0: ('*', '#', 8),
+        1: (2, 4),
+        2: (1, 3, 5),
+        3: (2, 6),
+        4: (1, 5, 7),
+        5: (2, 4, 6, 8),
+        6: (3, 5, 9),
+        7: ('*', 4, 8),
+        8: (0, 5, 7, 9),
+        9: ('#', 6, 8)
     }
 
-def solution(numbers, hand):
+    x_y_location = {
+        '*': (0, 0),
+        0: (1, 0),
+        '#': (2, 0),
+        7: (0, 1),
+        8: (1, 1),
+        9: (2, 1),
+        4: (0, 2),
+        5: (1, 2),
+        6: (2, 2),
+        1: (0, 3),
+        2: (1, 3),
+        3: (2, 3)
+    }
+    
     answer = ""
     
-    left = [1, 4, 7, '*']
-    right = [3, 6, 9, '#']
-    mid = [0, 2, 5, 8]
+    left = (1, 4, 7, '*')
+    right = (3, 6, 9, '#')
+    mid = (0, 2, 5, 8)
     
     location = {
         'left': '*',
@@ -29,39 +44,46 @@ def solution(numbers, hand):
         if n in left:
             location['left'] = n
             answer += "L"
+            continue
         if n in right:
             location['right'] = n
             answer += "R"
+            continue
         if n in mid:
-            if location['left'] in keypad[n] and location['right'] not in keypad[n]:
+            if (location['left'] in keypad[n]) and (location['right'] not in keypad[n]):
                 location['left'] = n
                 answer += "L"
                 continue
-            if location['right'] in keypad[n] and location['left'] not in keypad[n]:
+            if (location['right'] in keypad[n]) and (location['left'] not in keypad[n]):
                 location['right'] = n
                 answer += "R"
                 continue
-            if location['left'] and location['right'] in keypad[n]:
+            if (location['left'] in keypad[n]) and (location['right'] in keypad[n]):
                 location[hand] = n
                 if hand == 'left':
                     answer += "L"
+                    continue
                 if hand == 'right':
                     answer += "R"
-                continue
-            if location['left'] and location['right'] not in keypad[n]:
-                D = False
-                for i in keypad[location['left']]:
-                    if i in keypad[n]:
-                        location['left'] = n
+                    continue
+            if (location['left'] not in keypad[n]) and (location['right'] not in keypad[n]):
+                left_dist = int()
+                right_dist = int()
+                left_dist = abs(x_y_location[n][0] - x_y_location[location['left']][0]) + abs(x_y_location[n][1] - x_y_location[location['left']][1])
+                right_dist = abs(x_y_location[n][0] - x_y_location[location['right']][0]) + abs(x_y_location[n][1] - x_y_location[location['right']][1])
+                
+                if left_dist < right_dist:
+                    answer += "L"
+                    continue
+                if left_dist > right_dist:
+                    answer += "R"
+                    continue
+                if left_dist == right_dist:
+                    if hand == 'left':
                         answer += "L"
-                        D = True
-                        break
-                if D == False:
-                    for j in keypad[location['right']]:
-                        if j in keypad[n]:
-                            location['right'] = n
-                            answer += "R"
-                            break
+                        continue
+                    if hand == 'right':
+                        answer += "R"                    
     print(answer)
     return answer
 
